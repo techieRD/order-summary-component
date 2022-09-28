a81f3bff51a21c389060f7542dd312c67147a08d
techierd <angiegardi@yahoo.com>
2022-09-27 21:57:31 -0500

    added more content and buttons

---
+++ fp6.js
@@ -0,0 +1,14 @@
+// Functional Programming for Beginners Exercise
+
+const reviews = [4.5, 4.0, 5.0, 2.0, 1.0, 5.0, 3.0, 4.0, 1.0, 5.0, 4.5, 3.0, 2.5, 2.0];
+
+// 1. Using the reduce function, create an object that
+// has properties for each review value, where the value
+// of the property is the number of reviews with that score.
+// for example, the answer should be shaped like this:
+// { 4.5: 1, 4: 2 ...}
+// 
+// TIP: checkout computed properties discussed here:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#Computed_property_names
+// solution can be found at:
+// https://jsbin.com/dehiqux/1/edit?js,console
